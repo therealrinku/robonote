bd674ce146cff4bcb3f55f3eefd26ac50e6c4326
rinkuuu <backupfb2032@gmail.com>
2021-02-28 18:01:09 +0545

    dummy lines reduced

--- src/Components/DummyLines.js
+++ src/Components/DummyLines.js
@@ -3,7 +3,7 @@ import React from "react";
 const DummyLines = ({ todosLength }) => {
   return (
     <div className="dummy--lines">
-      {[...Array(todosLength < 14 ? 14 - todosLength : 1)].map((_, i) => {
+      {[...Array(todosLength < 12 ? 12 - todosLength : 1)].map((_, i) => {
         return <li key={i}></li>;
       })}
     </div>
