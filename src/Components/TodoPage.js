980362f90ddde68935d0f4dc398421f1ece5390f
rinkuuu <backupfb2032@gmail.com>
2021-03-01 06:59:36 +0545

    updated a lot

--- src/Components/TodoPage.js
+++ src/Components/TodoPage.js
@@ -1,21 +1,55 @@
-import React from "react";
 import TodoDate from "./TodoDate";
 import TodoList from "./TodoList";
 import TodoAddForm from "./TodoAddForm";
 import DummyLines from "./DummyLines";
 import Loader from "./Loader";
+import { connect } from "react-redux";
+import * as todosActions from "../redux/todos/todosActions";
+import { useEffect, useState } from "react";
 
 const TodoPage = ({
   formatedDate,
-  UpdateTodo,
-  DeleteTodo,
-  AddTodo,
-  todos,
-  newTodo,
-  setNewTodo,
   datePlus,
   loading,
+  error,
+  allTodos,
+  currentUser,
+  deleteTodo,
+  updateTodo,
+  fetchTodos,
+  addTodo,
 }) => {
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState("");
+
+  const todosObject = allTodos.filter((data) => data.date === formatedDate);
+
+  useEffect(() => {
+    setNewTodo("");
+    if (!todosObject[0]?.date) {
+      fetchTodos(currentUser, formatedDate);
+    }
+  }, []);
+
+  const DeleteTodo = (todoValue) => {
+    deleteTodo(currentUser, formatedDate, allTodos, todoValue);
+  };
+
+  const UpdateTodo = (todoValue) => {
+    updateTodo(currentUser, formatedDate, allTodos, todoValue);
+  };
+
+  const AddNewTodo = (e) => {
+    e.preventDefault();
+    if (
+      newTodo.trim() !== "" &&
+      todosObject[0]?.todos.findIndex((todo) => todo.value === newTodo) < 0
+    ) {
+      addTodo(currentUser, formatedDate, allTodos, newTodo);
+      setNewTodo("");
+    }
+  };
+
   return (
     <main className="todo--page">
       {loading ? <Loader /> : null}
@@ -24,11 +58,33 @@ const TodoPage = ({
       <TodoAddForm
         newTodo={newTodo}
         setNewTodo={setNewTodo}
-        AddTodo={AddTodo}
+        AddTodo={AddNewTodo}
       />
       <DummyLines todosLength={todos.length} />
     </main>
   );
 };
 
-export default TodoPage;
+const mapStateToProps = (state) => {
+  return {
+    error: state.todos.error,
+    loading: state.todos.loading,
+    allTodos: state.todos.todos,
+    currentUser: state.user.currentUser,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    deleteTodo: (user, date, fulltodolist, todovalue) =>
+      dispatch(todosActions.DELETE_TODO(user, date, fulltodolist, todovalue)),
+    updateTodo: (user, date, fulltodolist, todovalue) =>
+      dispatch(todosActions.UPDATE_TODO(user, date, fulltodolist, todovalue)),
+    addTodo: (user, date, fulltodolist, newtodo) =>
+      dispatch(todosActions.ADD_TODO(user, date, fulltodolist, newtodo)),
+    fetchTodos: (currentUser, date) =>
+      dispatch(todosActions.FETCH_TODOS(currentUser, date)),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(TodoPage);
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,30 +1,15 @@
-import React, { useState, useEffect } from "react";
+import { useState } from "react";
 import { connect } from "react-redux";
 import Arrows from "../components/Arrows";
 import Footer from "../components/Footer";
 import Nav2 from "../components/Nav2";
 import TodoPage from "../components/TodoPage";
 import * as userActions from "../redux/user/userActions";
-import * as todosActions from "../redux/todos/todosActions";
 import moment from "moment";
 import { Redirect } from "react-router-dom";
 
-const Homepage = ({
-  CURRENTUSER,
-  SIGNOUT,
-  FULLTODOLIST,
-  FETCH_TODOS,
-  LOADING,
-  ERROR,
-  ADD_TODO,
-  UPDATE_TODO,
-  DELETE_TODO,
-}) => {
-  const [newTodo, setNewTodo] = useState("");
+const Homepage = ({ signOut, currentUser }) => {
   const [datePlus, setDatePlus] = useState(0);
-  const formatedDate = moment(
-    moment(new Date()).add({ days: datePlus })
-  ).format("dddd MMM Do YYYY");
 
   const createDate = (addNum) => {
     return moment(moment(new Date()).add({ days: addNum })).format(
@@ -40,59 +25,24 @@ const Homepage = ({
     datePlus + 3,
   ].map((e) => createDate(e));
 
-  const todosObject = FULLTODOLIST.filter((data) => data.date === formatedDate);
-
-  useEffect(() => {
-    setNewTodo("");
-    if (!todosObject[0]?.date) {
-      FETCH_TODOS(CURRENTUSER, formatedDate);
-    }
-  }, [formatedDate]);
-
-  const DeleteTodo = (todoValue) => {
-    DELETE_TODO(CURRENTUSER, formatedDate, FULLTODOLIST, todoValue);
-  };
-
-  const UpdateTodo = (todoValue) => {
-    UPDATE_TODO(CURRENTUSER, formatedDate, FULLTODOLIST, todoValue);
-  };
-
-  const AddNewTodo = (e) => {
-    e.preventDefault();
-    if (
-      newTodo.trim() !== "" &&
-      todosObject[0]?.todos.findIndex((todo) => todo.value === newTodo) < 0
-    ) {
-      ADD_TODO(CURRENTUSER, formatedDate, FULLTODOLIST, newTodo);
-      setNewTodo("");
-    }
-  };
-
   return (
     <div className="homepage">
-      <Nav2 currentUser={CURRENTUSER} SIGNOUT={SIGNOUT} />
+      <Nav2 currentUser={currentUser} SIGNOUT={signOut} />
 
       <div className="daily--todo-list">
         {dateArrays.map((e) => {
           return (
             <TodoPage
               key={new Date() * Math.random()}
-              loading={LOADING}
-              todos={todosObject[0]?.todos || []}
               formatedDate={e}
-              newTodo={newTodo}
-              setNewTodo={setNewTodo}
               datePlus={datePlus}
-              AddTodo={AddNewTodo}
-              UpdateTodo={UpdateTodo}
-              DeleteTodo={DeleteTodo}
             />
           );
         })}
       </div>
 
       <Arrows setDatePlus={setDatePlus} />
-      {!CURRENTUSER ? <Redirect to="/" /> : null}
+      {!currentUser ? <Redirect to="/" /> : null}
       <Footer />
     </div>
   );
@@ -100,24 +50,13 @@ const Homepage = ({
 
 const mapStateToProps = (state) => {
   return {
-    ERROR: state.todos.error,
-    LOADING: state.todos.loading,
-    FULLTODOLIST: state.todos.todos,
-    CURRENTUSER: state.user.currentUser,
+    currentUser: state.user.currentUser,
   };
 };
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    DELETE_TODO: (user, date, fulltodolist, todovalue) =>
-      dispatch(todosActions.DELETE_TODO(user, date, fulltodolist, todovalue)),
-    UPDATE_TODO: (user, date, fulltodolist, todovalue) =>
-      dispatch(todosActions.UPDATE_TODO(user, date, fulltodolist, todovalue)),
-    ADD_TODO: (user, date, fulltodolist, newtodo) =>
-      dispatch(todosActions.ADD_TODO(user, date, fulltodolist, newtodo)),
-    FETCH_TODOS: (currentUser, date) =>
-      dispatch(todosActions.FETCH_TODOS(currentUser, date)),
-    SIGNOUT: () => dispatch(userActions.SIGNOUT()),
+    signOut: () => dispatch(userActions.SIGNOUT()),
   };
 };
 
