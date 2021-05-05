279477cd34c1c2fe1a593a09f533633652e21f7f
therealrinku <backupfb2032@gmail.com>
2021-05-05 16:11:06 +0545

    little updae

--- src/components/LandingPageMain.js
+++ src/components/LandingPageMain.js
@@ -7,16 +7,20 @@ const LandingPageMain = () => {
   return (
     <main className="landing-main">
       <section>
-        <img src={MainImage} alt="main" />
+        <div>
+          <img src={MainImage} alt="main" />
+        </div>
+
+        <div>
+          <h2>
+            Fallora is your new daily task manager
+            <br /> to take you to the moon.
+          </h2>
+          <button onClick={() => history.push("/signup")}>Start 15 days free trial</button>
+        </div>
       </section>
 
-      <section>
-        <h2>
-          Fallora is your new daily task manager
-          <br /> to take you to the moon.
-        </h2>
-        <button onClick={() => history.push("/signup")}>Start 15 days free trial</button>
-      </section>
+      <section></section>
     </main>
   );
 };
