098def94576ceaeaeb3d63f74b9a6b2120c46658
therealrinku <backupfb2032@gmail.com>
2021-05-05 15:40:34 +0545

    little update

--- src/components/LandingPageMain.js
+++ src/components/LandingPageMain.js
@@ -1,5 +1,24 @@
+import MainImage from "../assets/main.svg";
+import { useHistory } from "react-router-dom";
+
 const LandingPageMain = () => {
-  return <main></main>;
+  const history = useHistory();
+
+  return (
+    <main className="landing-main">
+      <section>
+        <img src={MainImage} alt="main" />
+      </section>
+
+      <section>
+        <h2>
+          Fallora is your new daily task manager
+          <br /> to take you to the moon.
+        </h2>
+        <button onClick={() => history.push("/signup")}>Start 15 days free trial</button>
+      </section>
+    </main>
+  );
 };
 
 export default LandingPageMain;
