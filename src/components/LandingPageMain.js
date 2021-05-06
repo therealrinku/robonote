f1e4eaf5eebf14747458652b6a1ebb30d94b7635
therealrinku <backupfb2032@gmail.com>
2021-05-06 08:34:20 +0545

    reviews section added

--- src/components/LandingPageMain.js
+++ src/components/LandingPageMain.js
@@ -1,6 +1,6 @@
 import MainImage from "../assets/main.svg";
 import { useHistory } from "react-router-dom";
-import { FcExpand, FcBarChart, FcUpRight } from "react-icons/all";
+import FiveStarImage from "../assets/Stars.png";
 
 const LandingPageMain = () => {
   const history = useHistory();
@@ -23,18 +23,29 @@ const LandingPageMain = () => {
 
       <section>
         <div>
-          <FcExpand />
-          <p>Simple and Easy to use design</p>
+          <img src={FiveStarImage} alt="main" />
+          <p>“I’ve tried a million other todo apps and can’t find anything else I like more than Falloora!”</p>
         </div>
 
         <div>
-          <FcBarChart />
-          <p>Weekly report</p>
+          <img src={FiveStarImage} alt="main" />
+          <p>
+            “With your service, I can stay organized in a way that works for me. Yours is the most impactful tool I use.
+            Thank you so much for giving the world this magical helper.”
+          </p>
         </div>
 
         <div>
-          <FcUpRight />
-          <p>Drag and Drop functionality</p>
+          <img src={FiveStarImage} alt="main" />
+          <p>
+            “Many to-do apps have the feel of a project management tool. Falloora replaces pen and paper in a great way.
+            Thanks!”
+          </p>
+        </div>
+
+        <div>
+          <img src={FiveStarImage} alt="main" />
+          <p>“What makes it so great is that it’s clean and slick and not too complicated.”</p>
         </div>
       </section>
     </main>
