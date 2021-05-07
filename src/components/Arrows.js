2ea0b84aaddafcfe455b6db4112c2936948bfa2a
therealrinku <backupfb2032@gmail.com>
2021-05-07 09:30:42 +0545

    arrows update

--- src/components/Arrows.js
+++ src/components/Arrows.js
@@ -1,18 +1,18 @@
 import React from "react";
-import { AiOutlineArrowRight, AiOutlineArrowLeft } from "react-icons/all";
+import { CgChevronRight, CgChevronLeft } from "react-icons/all";
 
 const Arrows = ({ setDatePlus }) => {
   return (
     <div className="arrows">
       <section>
         <button onClick={() => setDatePlus((prev) => prev - 1)}>
-          <AiOutlineArrowLeft />
+          <CgChevronLeft />
         </button>
       </section>
 
       <section>
         <button onClick={() => setDatePlus((prev) => prev + 1)}>
-          <AiOutlineArrowRight />
+          <CgChevronRight />
         </button>
       </section>
     </div>
