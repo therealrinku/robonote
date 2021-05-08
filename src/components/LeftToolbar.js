078c5a904af4ce31539633e4019f28a1bf6de7c7
therealrinku <backupfb2032@gmail.com>
2021-05-08 13:42:48 +0545

    home button issue fix

--- src/components/LeftToolbar.js
+++ src/components/LeftToolbar.js
@@ -1,13 +1,18 @@
 import { VscHome, CgChevronLeft } from "react-icons/all";
 
 const LeftToolbar = ({ setDatePlus, setCurrentDate }) => {
+  const resetDate = () => {
+    setCurrentDate(new Date());
+    setDatePlus(0);
+  };
+
   return (
     <div className="toolbar left">
       <button onClick={() => setDatePlus((prev) => prev - 1)}>
         <CgChevronLeft />
       </button>
 
-      <button onClick={() => setCurrentDate(new Date())}>
+      <button onClick={resetDate}>
         <VscHome />
       </button>
     </div>
