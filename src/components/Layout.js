0aafb2744cfb2ade2fba3da134b505a10d400a15
therealrinku <backupfb2032@gmail.com>
2021-05-09 12:23:49 +0545

    navbars combined

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,12 +1,11 @@
 import { connect } from "react-redux";
 import Footer from "./Footer";
-import Nav1 from "./Nav1";
-import Nav2 from "./Nav2";
+import Navbar from "./Navbar";
 
 const Layout = ({ children, currentUser }) => {
   return (
     <>
-      {currentUser ? <Nav2 currentUser={currentUser} /> : <Nav1 />}
+      <Navbar currentUser={currentUser} />
       {children}
       <Footer />
     </>
