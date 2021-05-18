78e5a10f7b97cf6f3b659527dea5252a4c5ef447
therealrinku <backupfb2032@gmail.com>
2021-05-18 17:32:52 +0545

    hover effect added

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "18c5f7cd582b2ff59a9c";
+/******/ 	var hotCurrentHash = "be94d116dc0834674005";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
