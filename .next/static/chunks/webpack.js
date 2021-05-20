807ee4bb80c8ecb2c4993632adf50e7757cfac76
therealrinku <backupfb2032@gmail.com>
2021-05-20 15:35:26 +0545

    little improve

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "8ccc22f03f40ed0a0107";
+/******/ 	var hotCurrentHash = "ffb4b505039808bb9027";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
