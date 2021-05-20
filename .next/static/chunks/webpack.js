006850134c87c2d1bee27e8c51a2b144d94de819
therealrinku <backupfb2032@gmail.com>
2021-05-20 14:33:29 +0545

    homepage styles and layout updated

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "ff1322172d4725332760";
+/******/ 	var hotCurrentHash = "929e49d09ebc231b8226";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- components/HomeSectionOne.js
+++ components/HomeSectionOne.js
@@ -7,11 +7,16 @@ const HomepageOne = () => {
 
   return (
     <div className={homepageStyles.homeSectionOne}>
-      <img src={MainImage} alt="test" />
-      <h2>Snaptask is your new todo list manager for maximum productivity.</h2>
       <section>
-        <input type="text" placeholder="Type your email here" />
-        <button onClick={() => router.push("/signup")}>Signup</button>
+        <h2>Snaptask is your new todo list manager to help you acheive your goals.</h2>
+        <div>
+          <input type="text" placeholder="Type your email here" />
+          <button onClick={() => router.push("/signup")}>Start for free</button>
+        </div>
+      </section>
+
+      <section>
+        <img src={MainImage} alt="test" />
       </section>
     </div>
   );
