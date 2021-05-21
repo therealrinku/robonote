c66b13d052b1c8e5d8e566be601b16ec635707b8
therealrinku <backupfb2032@gmail.com>
2021-05-21 12:10:53 +0545

    date generate done

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "0d3193f25885cb22e4ca";
+/******/ 	var hotCurrentHash = "43ce9dc3c31ff80aa5b5";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- components/TodosBoard.js
+++ components/TodosBoard.js
@@ -1,17 +1,28 @@
 import todosBoardStyles from "../styles/TodosBoard.module.css";
 import TodoBoard from "./TodoBoard";
+import moment from "moment";
+import { useState } from "react";
 
 const TodosBoard = () => {
-  const boards = ["sunday", "monday", "tuesday", "wednesday"];
   const todos = [
     ["brush", "ass"],
     ["headache", "dunk"],
     ["different asses and bigger boobs if my eyse are black or blues", "testes"],
     ["tetth ahce", "zello is my best asest better than bloddy bitcoin"],
   ];
+
+  //date generate
+  const [currentDate, setCurrentDate] = useState(new Date());
+  const [datePlus, setDatePlus] = useState(0);
+  const createDate = (daysToAdd) => {
+    return moment(currentDate).add({ days: daysToAdd }).format("dddd, MMMM D YYYY");
+  };
+
+  const dates = [datePlus, datePlus + 1, datePlus + 2, datePlus + 3].map((e) => createDate(e));
+
   return (
     <div className={todosBoardStyles.todosBoard}>
-      {boards.map((e, i) => {
+      {dates.map((e, i) => {
         return (
           <section key={i}>
             <TodoBoard todos={todos[i]} todosDate={e} />
