f358e280e68bd4d5be32314a7091b8bba0dccde0
therealrinku <backupfb2032@gmail.com>
2021-05-24 11:54:42 +0545

    context passed

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,16 @@
+import { useState } from "react";
 import Layout from "../components/Layout";
 import "../styles/globals.css";
+import UserContext from "../userContext";
 
 export default function MyApp({ Component, pageProps }) {
+  const [email, setEmail] = useState("");
+
   return (
     <Layout>
-      <Component {...pageProps} />
+      <UserContext.Provider value={{ email, setEmail }}>
+        <Component {...pageProps} />
+      </UserContext.Provider>
     </Layout>
   );
 }
--- userContext.js
+++ userContext.js
@@ -1,4 +1,5 @@
 import React from "react";
 
-const UserContext = React.createContext({ email: "" });
+const UserContext = React.createContext({ email: "", setEmail: () => {} });
+
 export default UserContext;
