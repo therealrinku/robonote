56912a787a5ab569335fd94b11c57ef2106d14be
therealrinku <backupfb2032@gmail.com>
2021-05-26 15:08:11 +0545

    hiding login and signup button on their routes

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,14 +1,26 @@
 import navbarStyles from "../styles/Navbar.module.css";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 export default function Navbar() {
+  const router = useRouter();
+  const { pathname } = router;
+
   return (
     <nav className={navbarStyles.nav}>
       <ul>
         <Link href="/">Snaptask</Link>
       </ul>
 
-      <ul>
+      <ul
+        className={
+          pathname === "/signup"
+            ? navbarStyles.hideSignupLink
+            : pathname === "/login"
+            ? navbarStyles.hideLoginLink
+            : null
+        }
+      >
         <Link href="/login">Login</Link>
         <Link href="/signup">Signup</Link>
       </ul>
