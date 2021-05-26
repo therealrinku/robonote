067fe670171a8a84970da7586528a3af8031aea1
therealrinku <backupfb2032@gmail.com>
2021-05-26 14:43:42 +0545

    quote section removed

--- components/LandingPage.js
+++ components/LandingPage.js
@@ -43,23 +43,6 @@ export default function LandingPage() {
     },
   ];
 
-  //quote
-  const quotes = [
-    {
-      text: "There is no greatness where there is not simplicity, goodness, and truth.",
-      author: "-Leo Tolstoy, 1828",
-    },
-    { text: "Simplicity is the ultimate sophistication.", author: "-Leonardo da Vinci, 1452" },
-    { text: "The simplest things are often the truest.", author: "-Richard Bach, 1936" },
-    {
-      text: "There are two ways to be rich: One is by acquiring much, and the other is by desiring little.",
-      author: "-Jackie French Koller, 1948",
-    },
-  ];
-
-  //randomly selecting quote
-  const selectedQuote = quotes[Math.floor(Math.random() * (3 - 0 + 1)) + 0];
-
   return (
     <>
       <Navbar />
@@ -132,11 +115,6 @@ export default function LandingPage() {
             );
           })}
         </div>
-      </div>
-
-      <div className={landingPageStyles.sectionFour}>
-        <h3>“{selectedQuote.text}”</h3>
-        <p>{selectedQuote.author}</p>
       </div>
 
       <footer className={landingPageStyles.footer}>
